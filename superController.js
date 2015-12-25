6c393ba82035e793c0bf56268dd0f8fe7b0baecf
ravkovskiy <kos1992@mail.ru>
2015-12-25 14:49:47 +0300

    полностью работают галочки

--- superController.js
+++ superController.js
@@ -5,11 +5,34 @@ function DealListCtrl ($scope) {
 			$scope.deals.indexOf(textInput.value) == -1) {$scope.deals
 			.push(textInput.value);
 			textInput.value='';
+			document.getElementById('checkboxAll').checked=false;
 		}
 	}
-	$scope.checkedCtrl=function($event, chec) {
-		if($event.currentTarget.id == 'checkboxAll') alert(1);
-		alert(chec.checked);
+	$scope.checkedAllCtrl=function($event) {
+		if($event.currentTarget.checked == true) {
+			for(var i=0; i < $scope.deals.length; i++) {
+				var check=document.getElementById('checkbox'+i);
+				check.checked=true;
+				check.parentNode.nextSibling.nextSibling.setAttribute('style','text-decoration:line-through; color:#ddd');
+			}
+		}
+		else {
+			for(var i=0; i < $scope.deals.length; i++) {
+				var check=document.getElementById('checkbox'+i);
+				check.checked=false;
+				check.parentNode.nextSibling.nextSibling.setAttribute('style','text-decoration:none; color:black');
+			}
+		}
+
+	}
+	$scope.checkedCtrl=function($event) {
+		if($event.currentTarget.checked == false) {
+			document.getElementById($event.currentTarget.id).parentNode.nextSibling.nextSibling.setAttribute('style','text-decoration:none; color:black');
+		}
+		if($event.currentTarget.checked == true) {
+			document.getElementById($event.currentTarget.id).parentNode.nextSibling.nextSibling.setAttribute('style','text-decoration:line-through; color:#ddd');
+		}
+		if($event.currentTarget.checked == false && document.getElementById('checkboxAll').checked ==true) document.getElementById('checkboxAll').checked=false;
 	}
 }
 
