759a69fe6a65ddfe2afdc208787eb2b270a2e0c6
ravkovskiy <kos1992@mail.ru>
2015-12-25 11:18:38 +0300

    сделал добавление заданий

--- superController.js
+++ superController.js
@@ -1,7 +1,6 @@
 function DealListCtrl ($scope) {
 	$scope.deals=['Написать продолжение кода', 'Принять душ', 'df'];
 	$scope.eventsCtrl=function($event) {
-		alert(1);
 		if($event.keyCode == '13' && !textInput.value == '' && 
 			$scope.deals.indexOf(textInput.value) == -1) {$scope.deals
 			.push(textInput.value);
