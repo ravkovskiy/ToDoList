05ae1c268e6bc8a27fe298a7033f71bd4c8ffd16
ravkovskiy <kos1992@mail.ru>
2015-12-27 11:34:20 +0300

    теперь deals хранит объекты: текст+ свойство выполненности
    Продолжаю работать с файлами index, app, controllers

--- controllers.js
+++ controllers.js
@@ -1,11 +1,86 @@
 function DealListCtrl ($scope) {
-	$scope.deals=['Написать продолжение кода', 'Принять душ', 'df'];
+
+	$scope.deals=[];
+
 	$scope.eventsCtrl=function($event) {
-		alert(1);
+
+		$scope.dealsText=[];
+		for(var i=0; i<$scope.deals.length; i++) {
+			$scope.dealsText[i]=$scope.deals[i].textToDo;
+		}
+
+
 		if($event.keyCode == '13' && !textInput.value == '' && 
-			$scope.deals.indexOf(textInput.value) == -1) {$scope.deals
-			.push(textInput.value);
+			$scope.dealsText.indexOf(textInput.value) == -1) {$scope.deals.unshift({'textToDo': textInput.value, 'done': 'false'});
 			textInput.value='';
+			document.getElementById('checkboxAll').checked=false;
+			if(document.getElementById('img').style != 'display: none') {
+				document.getElementById('img').setAttribute('style', 'display: none');
+			}
+		}	
+
+	}
+	$scope.checkedAllCtrl=function($event) {
+		if($event.currentTarget.checked == true) {
+			for(var i=0; i < $scope.deals.length; i++) {
+				var check=document.getElementById('checkbox'+i);
+				check.checked=true;
+				check.parentNode.nextSibling.nextSibling.setAttribute('style',
+					'text-decoration:line-through; transition: all .03s; color: #259433');
+				$scope.deals[i].done='true';
+			}
+		}
+		else {
+			for(var i=0; i < $scope.deals.length; i++) {
+				var check=document.getElementById('checkbox'+i);
+				check.checked=false;
+				check.parentNode.nextSibling.nextSibling.setAttribute('style',
+					'text-decoration:none; transition: all .03s;');
+				$scope.deals[i].done='false';
+			}
+		}
+
+	}
+	$scope.checkedCtrl=function($event) {
+		if($event.currentTarget.checked == false) {
+			$event.currentTarget.parentNode.nextSibling.nextSibling.setAttribute('style',
+				'text-decoration:none; transition: all .03s;');
+			$scope.deals[$event.currentTarget.id.slice(8)].done='false';
+		}
+		if($event.currentTarget.checked == true) {
+			$event.currentTarget.parentNode.nextSibling.nextSibling.setAttribute('style',
+				'text-decoration:line-through; transition: all .03s; color: #259433');
+			$scope.deals[$event.currentTarget.id.slice(8)].done='true';
+
+			var mass=[];
+		
+			for(var i=0; i < $scope.deals.length; i++) {
+				mass.push(document.getElementById('checkbox'+i).checked);
+			};
+			if(mass.indexOf(false) == -1) document.getElementById('checkboxAll').checked=true;
+		}
+
+		if($event.currentTarget.checked == false && document.getElementById('checkboxAll').checked ==true) {
+			document.getElementById('checkboxAll').checked=false;
+		}
+	}
+
+	$scope.closeCtrl=function($event, deal) {
+		var mass=[];
+		
+		for(var i=0; i < $scope.deals.length; i++) {
+			mass.push(document.getElementById('checkbox'+i).checked);
+		};
+
+		mass.splice($scope.deals.indexOf(deal),1);
+
+		$scope.deals.splice($scope.deals.indexOf(deal),1);
+
+
+		if(mass.indexOf(false) == -1 && mass.length > 0) document.getElementById('checkboxAll').checked=true;
+
+		if($scope.deals.length == 0) {
+			document.getElementById('img').setAttribute('style', '');
 		}
 	}
 }
--- superController.js
+++ superController.js
@@ -1,5 +1,7 @@
 function DealListCtrl ($scope) {
+	
 	$scope.deals=[];
+
 	$scope.eventsCtrl=function($event) {
 		if($event.keyCode == '13' && !textInput.value == '' && 
 			$scope.deals.indexOf(textInput.value) == -1) {$scope.deals
