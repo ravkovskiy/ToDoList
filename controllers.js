c35081c45ce7d2b3a5db89a7d655444f23cb84d2
ravkovskiy <kos1992@mail.ru>
2016-01-05 22:36:11 +0300

    данные теперь сохраняются в хранилище, добавлены несколько файлов со стилями, для сравнения

--- controllers.js
+++ controllers.js
@@ -1,7 +1,8 @@
+
 angular.module('ToDo', []).
   filter('dealsDone', function() {
     return function(deals, dealsFilter) {
-    	if(dealsFilter=='all') return deals;
+    	if(dealsFilter == 'all') return deals;
        	var mas=[];
     	for (var i=0; i<deals.length; i++) {
     		if(deals[i].done == dealsFilter) {
@@ -18,28 +19,41 @@ angular.module('ToDo', []).
 /*Объявляем функцию контроллер*/
 function DealListCtrl ($scope) {
 
-	$scope.deals=[];
 	$scope.dealsFilter='all';
+
+	if(restoreSettings()){
+		$scope.deals=[];
+		
+	}
+	else {
+		$scope.deals=[];
+		for(var i=0; i<localStorage['numbers.deals']; i++) {
+			$scope.deals[i]={'textToDo': localStorage['text.to.do.'+i],
+		    'done': 'true' == localStorage['done.'+i]};
+		}
+		document.getElementById('checkboxAll').checked=(localStorage['checkbox.all'] == 'true');
+		document.getElementById('img').setAttribute('style', 'display: '+localStorage['display.img']);
+	}
+
 /*При вводе новых дел выполняется функция eventsCtrl */
 	$scope.eventsCtrl=function($event) {
-
 		$scope.dealsText=[];
-
 		for(var i=0; i<$scope.deals.length; i++) {
 			$scope.dealsText[i]=$scope.deals[i].textToDo;
 		}
-
 		/* textInput это id поля ввода */
 		if($event.keyCode == '13' && !textInput.value == '' && 
-			$scope.dealsText.indexOf(textInput.value) == -1) {$scope.deals.unshift({'textToDo': textInput.value, 'done': false});
+			$scope.dealsText.indexOf(textInput.value) == -1) {
+			$scope.deals.unshift({'textToDo': textInput.value, 'done': false});
 			textInput.value='';
 			document.getElementById('checkboxAll').checked=false;
 			if(document.getElementById('img').style != 'display: none') {
 				document.getElementById('img').setAttribute('style', 'display: none');
 			}
+			savedSettings($scope.deals);
 		}	
-
 	}
+
 	$scope.checkedAllCtrl=function($event) {
 		if($event.currentTarget.checked == true) {
 			for(var i=0; i < $scope.deals.length; i++) {
@@ -51,11 +65,11 @@ function DealListCtrl ($scope) {
 				$scope.deals[i].done=false;		
 			}
 		}
-
+		savedSettings($scope.deals);
 	}
+
 	$scope.checkedCtrl=function($event) {
 		if($event.currentTarget.checked == false) {
-			
 			$scope.deals[$event.currentTarget.id.slice(8)].done=false;
 			document.getElementById('checkboxAll').checked=false;
 		}
@@ -68,73 +82,74 @@ function DealListCtrl ($scope) {
 			};
 		    
 		}
-
-		/*Следующий код сортирует массив дел, чтобы выполненные дела нахлдились внизу списка*/
-		var oldDeals=[];
-
-		for(var i=0; i<$scope.deals.length; i++) {
-			oldDeals[i]={};
-			oldDeals[i].textToDo=$scope.deals[i].textToDo;
-			oldDeals[i].done=$scope.deals[i].done;
-		}
 		
-		var j=oldDeals.length-1;
-		var k=0;
-
-		for(var i=j; i>=0; i--) {
-			if(oldDeals[i].done == true) {
-				$scope.deals[j].textToDo=oldDeals[i].textToDo;
-				$scope.deals[j].done=oldDeals[i].done;
-				document.getElementById('checkbox'+j).checked=true;
-				j--;
-				k++;
-			}
-		}
-		j=oldDeals.length-1-k;	
-		for(var i=oldDeals.length-1; i>=0; i--) {
-			if(oldDeals[i].done == false) {
-				$scope.deals[j].textToDo=oldDeals[i].textToDo;
-				$scope.deals[j].done=oldDeals[i].done;
-				document.getElementById('checkbox'+j).checked=false;
-				
-				j--;
-			}
-		}
+		savedSettings($scope.deals);
 	}
 
 	$scope.closeCtrl=function($event, deal) {
+
 		var mass=[];
 		
 		for(var i=0; i < $scope.deals.length; i++) {
-			mass.push(document.getElementById('checkbox'+i).checked);
+			if(!document.getElementById('checkbox'+i) == undefined) {
+				mass.push(document.getElementById('checkbox'+i).checked);
+			}
+			
 		};
-
 		mass.splice($scope.deals.indexOf(deal),1);
-
 		$scope.deals.splice($scope.deals.indexOf(deal),1);
-
-
 		if(mass.indexOf(false) == -1 && mass.length > 0) document.getElementById('checkboxAll').checked=true;
-
 		if($scope.deals.length == 0) {
 			document.getElementById('img').setAttribute('style', '');
 			document.getElementById('checkboxAll').checked=false;
 		}
+		savedSettings($scope.deals, $scope.dealsFilter);
 	}
+
 	$scope.all=function() {
 		$scope.dealsFilter='all';
+		savedSettings($scope.deals);
 	}
+
 	$scope.notDone=function() {
 		$scope.dealsFilter= false;
+		savedSettings($scope.deals);
 	}
+
 	$scope.done=function() {
 		$scope.dealsFilter= true;
+		savedSettings($scope.deals);
 	}
+
 	$scope.closeAll=function() {
-		$scope.deals=[];
-		document.getElementById('checkboxAll').checked=false;
-		document.getElementById('img').setAttribute('style', 'display: block');
+		if($scope.dealsFilter == 'all') {
+			$scope.deals=[];
+			document.getElementById('checkboxAll').checked=false;
+			document.getElementById('img').setAttribute('style', 'display: block');
+			savedSettings($scope.deals);
+		}
+		if($scope.dealsFilter == true) {
+			for(var i=0; i<$scope.deals.length; i++) {
+				if($scope.deals[i].done == true) {
+					$scope.deals.splice(i, 1);
+					--i;
+				}
+			}
+			document.getElementById('checkboxAll').checked=false;
+		}
+		if($scope.dealsFilter == false) {
+			for(var i=0; i<$scope.deals.length; i++) {
+				if($scope.deals[i].done == false) {
+					$scope.deals.splice(i, 1);
+					--i;
+				}
+			}
+			if(!$scope.deals.length == 0) document.getElementById('checkboxAll').checked=true;
+		}
+		
+		
 	}
+
 }
 
 
